fc7c7dbe89d6e1fb0f88ab722081cae971779beb
swdpcomputing <swdpcomputing@outlook.com>
2021-12-08 13:41:35 +0000

    feat(before-you-start): add use a different service page

--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const localPlanningDepartmentRouter = require('./local-planning-department');
 const typeOfPlanningApplicationRouter = require('./type-of-planning-application');
-const useDiffrentServiceRouter = require('./use-a-different-service');
+const useADifferentServiceRouter = require('./use-a-different-service');
 
 const router = express.Router();
 router.use(localPlanningDepartmentRouter);
 router.use(typeOfPlanningApplicationRouter);
-router.use(useDiffrentServiceRouter);
+router.use(useADifferentServiceRouter);
 
 module.exports = router;
