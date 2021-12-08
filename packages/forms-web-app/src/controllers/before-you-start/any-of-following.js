4c714e9fa4f8b9f00bd6d4e2e45065a457e85239
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-12-08 11:21:48 +0000

    fix(as-3842): pr feedback + test fixes

--- packages/forms-web-app/src/controllers/before-you-start/any-of-following.js
+++ packages/forms-web-app/src/controllers/before-you-start/any-of-following.js
@@ -1,37 +1,16 @@
 /* eslint-disable consistent-return */
 const { VIEW } = require('../../lib/views');
 
-const routingOptions = (option) => {
-  switch (option) {
-    case 'a_listed_building':
-    case 'major_dwellings':
-    case 'major_general_industry_storage_warehousing':
-    case 'major_travelling_and_caravan_pitches':
-    case 'major_retail_and_services':
-      return false;
-
-    default:
-      return false;
-  }
-};
-
 const routeUserOption = (options) => {
   if (typeof options === 'string' && options === 'none_of_these') {
     return true;
-  }
-
-  if (Array.isArray(options)) {
-    return options.some((option) => routingOptions(option));
   }
 
   return false;
 };
 
 const getAnyOfFollowing = async (req, res) => {
-  res.render(VIEW.BEFORE_YOU_START.ANY_OF_FOLLOWING, {
-    errors: {},
-    errorSummary: [],
-  });
+  res.render(VIEW.BEFORE_YOU_START.ANY_OF_FOLLOWING);
 };
 
 const postAnyOfFollowing = async (req, res) => {
