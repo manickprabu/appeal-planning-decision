f69edf463d85aa9098c74d5cea42843f98b77c14
swdpcomputing <swdpcomputing@outlook.com>
2021-12-10 13:44:26 +0000

    refactor(full-planning): refactored full-planning directories

--- packages/forms-web-app/__tests__/unit/routes/index.test.js
+++ packages/forms-web-app/__tests__/unit/routes/index.test.js
@@ -5,7 +5,7 @@ const homeRouter = require('../../../src/routes/home');
 const cookieRouter = require('../../../src/routes/cookies');
 const guidancePagesRouter = require('../../../src/routes/guidance-pages');
 const yourPlanningAppealRouter = require('../../../src/routes/your-planning-appeal');
-const beforeYouStartRouter = require('../../../src/routes/before-you-start');
+const fullPlanningRouter = require('../../../src/routes/full-planning/index');
 
 const checkDecisionDateDeadline = require('../../../src/middleware/check-decision-date-deadline');
 
@@ -30,7 +30,7 @@ describe('routes/index', () => {
     );
     expect(use).toHaveBeenCalledWith('/eligibility', checkDecisionDateDeadline, eligibilityRouter);
     expect(use).toHaveBeenCalledWith('/your-planning-appeal', yourPlanningAppealRouter);
-    expect(use).toHaveBeenCalledWith('/before-you-start', beforeYouStartRouter);
+    expect(use).toHaveBeenCalledWith('/before-you-start', fullPlanningRouter);
 
     expect(use.mock.calls.length).toBe(7);
   });
