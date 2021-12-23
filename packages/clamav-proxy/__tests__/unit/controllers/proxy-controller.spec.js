b5e4803d0b192c23301ab3038db11305ee259302
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-12-23 09:51:14 +0000

    feat(as-3650): update unit test + lib

--- packages/clamav-proxy/__tests__/unit/controllers/proxy-controller.spec.js
+++ packages/clamav-proxy/__tests__/unit/controllers/proxy-controller.spec.js
@@ -1,4 +1,4 @@
-jest.mock("../../../src/lib/clamd")
+jest.mock('../../../src/lib/clamd');
 const clamd = require('../../../src/lib/clamd');
 const controller = require('../../../src/controllers/proxy-controller');
 const { mockReq, mockRes } = require('../mocks');
@@ -8,61 +8,61 @@ describe('controllers/proxy-controller', () => {
   let res;
 
   beforeEach(() => {
-    req = mockReq()
-    res = mockRes()
+    req = mockReq();
+    res = mockRes();
 
     jest.resetAllMocks();
-  })
+  });
 
   it('should process file sent via form data', async () => {
-
-    clamd.sendFile.mockResolvedValue({
-      file: "",
+    const mockResponse = clamd.sendFile.mockResolvedValue({
+      file: '',
       isInfected: true,
-      viruses: []
-    })
+      viruses: [],
+    });
 
     const sampleFile = {
       title: 'sample',
-      buffer: Buffer.from([])
-    }
+      buffer: Buffer.from([]),
+    };
 
     const mockRequest = {
       ...req,
       files: {
-        file: [sampleFile]
-      }
+        file: [sampleFile],
+      },
     };
 
     await controller.processFile(mockRequest, res);
+    expect(mockResponse).toBeCalledTimes(1);
     expect(res.send).toHaveBeenCalledWith({
-      file: "",
+      file: '',
       isInfected: true,
-      viruses: []
-    })
+      viruses: [],
+    });
   });
 
   it('should return 400 for file is undefined', async () => {
     const mockRequest = {
       ...req,
       files: {
-        file: undefined
-      }
+        file: undefined,
+      },
     };
 
     await controller.processFile(mockRequest, res);
-    expect(res.sendStatus).toHaveBeenCalledWith(400)
+    expect(res.sendStatus).toHaveBeenCalledWith(400);
   });
 
   it('should return message after error', async () => {
     const mockRequest = {
       ...req,
       files: {
-        file: [undefined]
-      }
+        file: [undefined],
+      },
     };
 
     await controller.processFile(mockRequest, res);
-    expect(res.send).toHaveBeenCalledWith('error uploading file to clamav')
-  })
+    expect(res.send).toHaveBeenCalledWith('error uploading file to clamav');
+  });
 });
--- packages/clamav-proxy/src/lib/clamd.js
+++ packages/clamav-proxy/src/lib/clamd.js
@@ -2,6 +2,18 @@ const NodeClam = require('clamscan');
 const { Readable } = require('stream');
 const config = require('./config');
 
+const createClient = async () => {
+  return NodeClam.init({
+    removeInfected: true,
+    clamdscan: {
+      host: config.services.clamav.host || 'localhost',
+      port: config.services.clamav.port || 3310,
+      bypass_test: true,
+    },
+    preference: 'clamdscan',
+  });
+};
+
 const sendFile = async (file) => {
   if (typeof file === 'undefined') {
     throw new Error('invalid or empty file');
@@ -11,20 +23,12 @@ const sendFile = async (file) => {
     throw new Error('invalid file type, requires a buffer');
   }
 
+  const client = await createClient();
   const readableStream = Readable.from(file.toString());
-  const clamscan = await new NodeClam().init({
-    removeInfected: true,
-    clamdscan: {
-      host: config.services.clamav.host || 'localhost',
-      port: config.services.clamav.port || 3310,
-      bypass_test: true,
-    },
-    preference: 'clamdscan',
-  });
-
-  return clamscan.scanStream(readableStream);
+  return client.scanStream(readableStream);
 };
 
 module.exports = {
   sendFile,
+  createClient,
 };
