50741e95fb5267706e4f063f52d936df3e38cbf7
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2021-12-23 13:56:57 +0000

    feat: refactor code for house holder appeals
    refactor code

--- packages/e2e-tests/cypress/support/common/go-to-page/goToCheckYourAnswersPage.js
+++
@@ -1,4 +0,0 @@
-export const goToCheckYourAnswersPage = (options = {}) => {
-  cy.visit('/appellant-submission/check-answers', { failOnStatusCode: false, ...options });
-  cy.wait(Cypress.env('demoDelay'));
-};
