f80f23130214f952fb2683964f6a2c13f79b6fcf
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2021-12-23 19:41:12 +0000

    refactor: code change to resolve merge conflicts

--- packages/e2e-tests/cypress/integration/full-planning/appeals-service/appeal-form-task-list/appeal-form-task-list.js
+++ packages/e2e-tests/cypress/integration/full-planning/appeals-service/appeal-form-task-list/appeal-form-task-list.js
@@ -1,5 +1,4 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
-import { verifyPageTitle } from '../../../../../../../e2e-tests/cypress/support/common/verify-page-title';
 import {
   applicationStatusDetailedText,
   applicationStatusText,
@@ -13,13 +12,15 @@ import {
   statusUploadDocsFromPlanningApplication,
   statusUploadDocsForYourAppeal, statusCheckYourAnswers,
 } from '../../../../support/full-planning/appeals-service/page-objects/appeal-form-task-list-po';
+import { goToAppealsServicePage } from '../../../../support/common/go-to-page/goToAppealsServicePage';
+import { verifyPageTitle } from '../../../../support/common/verify-page-title';
 
 const pageHeading = 'Appeal a planning decision';
-const url = '/full-appeal/task-list';
+const url = 'full-appeal/task-list';
 const pageTitle = 'Appeal a planning decision - Appeal a planning decision - GOV.UK';
 
 Given('Appellant has been successful on their eligibility',()=> {
- cy.visit('http://localhost:9003/full-appeal/task-list');
+ goToAppealsServicePage(url);
 })
 When("they are on the 'Appeal a Planning Decision' page",()=> {
   verifyPageTitle(pageTitle);
