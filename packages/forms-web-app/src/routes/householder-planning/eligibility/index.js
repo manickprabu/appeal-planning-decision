7ce4730284fcb921ce877a8ad3b2b90ac99e12ec
nssdesikan <nssdesikan@rediffmail.com>
2022-01-04 09:32:44 +0000

    feat(as-4290): development of the feature and unit tests

--- packages/forms-web-app/src/routes/householder-planning/eligibility/index.js
+++ packages/forms-web-app/src/routes/householder-planning/eligibility/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const listedBuildingHouseholderRouter = require('./listed-building-householder');
+const grantedOrRefusedRouter = require('./granted-or-refused');
 
 router.use(listedBuildingHouseholderRouter);
+router.use(grantedOrRefusedRouter);
 
 module.exports = router;
