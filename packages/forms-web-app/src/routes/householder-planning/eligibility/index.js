9cfe75e6d557634f145e840f858a111c3c4b46d2
nssdesikan <nssdesikan@rediffmail.com>
2022-01-11 23:40:13 +0000

    feat(as-4290): include householder to the file and variable names

--- packages/forms-web-app/src/routes/householder-planning/eligibility/index.js
+++ packages/forms-web-app/src/routes/householder-planning/eligibility/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const listedBuildingHouseholderRouter = require('./listed-building-householder');
-const grantedOrRefusedRouter = require('./granted-or-refused');
+const grantedOrRefusedHouseholderRouter = require('./granted-or-refused-householder');
 
 router.use(listedBuildingHouseholderRouter);
-router.use(grantedOrRefusedRouter);
+router.use(grantedOrRefusedHouseholderRouter);
 
 module.exports = router;
